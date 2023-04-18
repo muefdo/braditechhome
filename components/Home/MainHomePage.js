81b105c12bf169ca894b592ca1daedc7539767f6
muefdo <mefe.dogan@tedu.edu.tr>
2023-04-18 23:51:15 +0300

    Made bradi.tech welcome page

---
+++ components/Home/MainHomePage.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import styles from "./MainHomePage.module.css"
+import tech from "./public/braditech.png"
+import cloud from "./public/bradicloud.png"
+import Image from 'next/image'
+import Link from 'next/link'
+
+const MainHomePage = () => {
+  return (
+    <div className={styles.Container}>
+      <div className={styles.leftSide}>
+        <div className={styles.ContentContainer}>
+          <Link href="https://my.bradi.tech/" legacyBehavior>
+            <a>
+              <Image src={tech} className={styles.Image} width={175} height={315} />
+            </a>
+          </Link>
+        </div>
+      </div>
+
+      <div className={styles.rightSide}>
+        <div className={styles.ContentContainer}>
+          <Link href="https://www.bradi.cloud/" legacyBehavior>
+            <a>
+              <Image src={cloud} className={styles.Image} width={175} height={315} />
+            </a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MainHomePage
